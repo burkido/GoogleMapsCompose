f33218366691934ae5dad21706d1d503d734e408
Burak Karaduman <karadumanburak000@gmail.com>
2023-01-26 15:38:13 +0300

    - bug fix

--- app/src/main/java/com/example/googlemapscompose/MainActivity.kt
+++ app/src/main/java/com/example/googlemapscompose/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.googlemapscompose.presentation.MapScreen
 import com.example.googlemapscompose.ui.theme.GoogleMapsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +16,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GoogleMapsComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MapScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GoogleMapsComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/googlemapscompose/presentation/MapScreen.kt
+++ app/src/main/java/com/example/googlemapscompose/presentation/MapScreen.kt
@@ -1,12 +1,28 @@
 package com.example.googlemapscompose.presentation
 
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MapScreen(
     viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
 ) {
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = if (viewModel.state.isCustomMap),
+                    contentDescription =
+                )
+            }
+        }
+    ) {
 
-
+    }
 
 }
