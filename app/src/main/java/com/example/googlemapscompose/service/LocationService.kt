3a17b92873acc35d185925331682e24b1ab515b7
Burak Karaduman <karadumanburak000@gmail.com>
2023-02-20 21:09:23 +0300

    - Location service added

---
+++ app/src/main/java/com/example/googlemapscompose/service/LocationService.kt
@@ -0,0 +1,104 @@
+package com.example.googlemapscompose.service
+
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.googlemapscompose.R
+import com.example.googlemapscompose.data.LocationClientImpl
+import com.example.googlemapscompose.domain.repository.LocationClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class LocationService : Service() {
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private lateinit var locationClient: LocationClient
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        locationClient = LocationClientImpl(
+            context = applicationContext,
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(applicationContext)
+        )
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action) {
+            ACTION_START_LOCATION_SERVICE -> { start() }
+            ACTION_STOP_LOCATION_SERVICE -> { stop() }
+        }
+
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        val notification = NotificationCompat.Builder(this, "location")
+            .setContentTitle("Tracking location...")
+            .setContentText("Location:")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setOngoing(true)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        locationClient
+            .getLocationUpdates(4000L)
+            .catch { e -> e.printStackTrace() }
+            .onEach { location ->
+                val lat = location.latitude.toString().takeLast(3)
+                val long = location.longitude.toString().takeLast(3)
+                val updatedNotification = notification.setContentText(
+                    "Location: ($lat, $long)"
+                )
+                notificationManager.notify(1, updatedNotification.build())
+            }
+            .launchIn(serviceScope)
+
+        startForeground(1, notification.build())
+    }
+
+    private fun stop() {
+        stopForeground(true)    // STOP_FOREGROUND_DETACH
+        stopSelf()
+    }
+
+//    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+//        when(intent?.action) {
+//            ACTION_START_LOCATION_SERVICE -> {
+//                serviceScope.launch {
+//                    locationClient.getLocationUpdates(1000L)
+//                }
+//            }
+//            ACTION_STOP_LOCATION_SERVICE -> {
+//                stopSelf()
+//            }
+//        }
+//
+//        return super.onStartCommand(intent, flags, startId)
+//    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    companion object {
+        const val TAG = "LocationService"
+        const val ACTION_START_LOCATION_SERVICE = "ACTION_START_LOCATION_SERVICE"
+        const val ACTION_STOP_LOCATION_SERVICE = "ACTION_STOP_LOCATION_SERVICE"
+    }
+}
